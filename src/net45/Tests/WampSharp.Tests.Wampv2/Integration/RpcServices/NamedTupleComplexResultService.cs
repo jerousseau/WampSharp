daedf716859a1303a47b577b967246a317f31a2c
Elad Zelingher <dark_leizar@walla.com>
2017-08-02 18:07:04 +0300

    Aweful workaround for running these tests

--- src/net45/Tests/WampSharp.Tests.Wampv2/Integration/RpcServices/NamedTupleComplexResultService.cs
+++ src/net45/Tests/WampSharp.Tests.Wampv2/Integration/RpcServices/NamedTupleComplexResultService.cs
@@ -7,6 +7,7 @@ namespace WampSharp.Tests.Wampv2.Integration.RpcServices
 {
     public class NamedTupleComplexResultService
     {
+        [WampProcedure("com.myapp.add_complex")]
         public (int c, int ci) AddComplex(int a, int ai, int b, int bi)
         {
             return (a + b, ai + bi);
